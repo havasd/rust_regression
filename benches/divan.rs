ecf1a0616131966617d8dd03445f0db1f423db04
Olaf Schlüter <tallinn1960@yahoo.de>
2024-06-15 07:34:51 +0200

    test code reorg for better selecting benchmarks

--- benches/divan.rs
+++ benches/divan.rs
@@ -1,35 +1,37 @@
-use day9_rust::{p1, p2};
-
 fn main() {
     // Run registered benchmarks.
     divan::main();
 }
 
-#[divan::bench]
-fn part1() {
-    p1(divan::black_box(include_str!("../input.txt",)));
+mod rust {
+    use day9_rust::{p1, p2};
+
+    #[divan::bench]
+    fn part1() {
+        p1(divan::black_box(include_str!("../input.txt",)));
+    }
+
+    #[divan::bench]
+    fn part2() {
+        p2(divan::black_box(include_str!("../input.txt",)));
+    }
 }
 
-#[divan::bench]
-fn part2() {
-    p2(divan::black_box(include_str!("../input.txt",)));
-}
-
-#[cfg(target_os = "macos")] 
+#[cfg(target_os = "macos")]
 mod swift {
     use day9::swift::{day9p1_rust_bridge, day9p2_rust_bridge};
     use swift_rs::{self, SRString};
     #[divan::bench]
-    fn part1_swift() {
+    fn part1() {
         unsafe {
             day9p1_rust_bridge(divan::black_box(SRString::from(include_str!(
                 "../input.txt",
             ))));
         }
     }
-    
+
     #[divan::bench]
-    fn part2_swift() {
+    fn part2() {
         unsafe {
             day9p2_rust_bridge(divan::black_box(SRString::from(include_str!(
                 "../input.txt",
@@ -37,4 +39,3 @@ mod swift {
         }
     }
 }
-
